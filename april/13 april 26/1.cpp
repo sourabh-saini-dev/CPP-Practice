91115cac6924017952eb6a1f101e7a6d9d31a4c7
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-04-13 01:23:57 +0530

    first commit

--- april/13 april 26/1.cpp
+++ april/13 april 26/1.cpp
@@ -1,1 +1,16 @@
-sdfgh
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of number";
+    cin>>n;
+    int sum = 0;
+    int last ;
+    int temp = n;
+    while(temp){
+        last = temp%10;
+        sum = sum*10+last;
+        temp/=10;
+    }
+    cout<<sum;
+}
