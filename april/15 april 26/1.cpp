9e92b7ea1b04e67e282c4760750a74f1b6ed9e5c
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-04-15 02:48:07 +0530

    1.cpp

--- april/15 april 26/1.cpp
+++ april/15 april 26/1.cpp
@@ -1,1 +1,18 @@
-#include
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter the value of number";
+    cin>>n;
+    vector<int> ans;
+    while(n){
+        int digit= n%10;
+        ans.push_back(digit*digit);
+        n/=10;
+    }
+   for(int i = 0; i<ans.size(); i++){
+    cout<<ans[i]<<" "
+   }
+   
+}
+;
