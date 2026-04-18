a704d66b238064a6c46f2d031b8dfbaed24e67df
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-04-18 14:52:26 +0530

    first commit

--- april/18/2.cpp
+++ april/18/2.cpp
@@ -1,1 +1,25 @@
-#in
+#include<bits/stdc++.h>
+using namespace std;
+bool linearsearch(char key, stirng str){
+    for(int i = 0; i<str.length(), i++){
+        if(str[i]==key){
+           return true;
+        }
+    }
+    return false;
+}
+ string removeduplicate(string str){
+        string res="";
+        for( int i = 0; i<str.length(); i++){
+            if(!linearsearch(str[i],res)){
+                res =rs+str[i];
+            
+            }
+        }
+        return res;
+ }
+int main(){
+    string str="prajjal";
+    cout<<removeduplicate(str);
+
+}
