cfee51c7d1caec5e20850860fc4f2c1110ff3cd2
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-05-06 01:00:09 +0530

    May

---
+++ May/6May26/1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+string reverseString(string n) {
+    int i = 0;
+    int j = n.length() - 1;
+
+    while (i < j) {
+        swap(n[i], n[j]);
+        i++;
+        j--;
+    }
+
+    return n;
+}
+
+int main() {
+    string n;
+
+    cout << "Enter the string: ";
+    cin >> n;
+
+    cout << reverseString(n);
+
+    return 0;
+}
