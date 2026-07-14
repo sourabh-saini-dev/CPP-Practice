f3e539fd5aa056694b5c6304495a313948d944d7
Sourabh Saini <sourabhsainiktp@gmail.com>
2026-07-15 00:53:59 +0530

    july

---
+++ July/15july26/1.cpp
@@ -0,0 +1,22 @@
+  
+#include <iostream>
+ using namespace std;
+  bool   duplicate( int arr[],int n){
+        for(int i  = 0; i<n; i++){
+         for(int j = i+1; j<n; j++){
+            if(arr[i] == arr[j]){
+              return true;
+            }
+         }
+        }
+        return false;
+     }
+
+int main()
+{ 
+    int arr []= {1,1,3,4,5};
+    int n = 5
+        cout<<duplicate(arr[],n);
+
+    return 0;
+} 
